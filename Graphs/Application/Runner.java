d74df1721f3b9f2ec303df712c927423f51f1157
k2devil <66827580+k2devil@users.noreply.github.com>
2024-06-01 15:46:09 +0530

    DFS

--- Graphs/Application/Runner.java
+++ Graphs/Application/Runner.java
@@ -2,7 +2,6 @@ package Graphs.Application;
 
 import java.util.ArrayList;
 
-import Graphs.BFS.Bfs;
 import Graphs.DFS.Dfs;
 import Graphs.Definition.CreateGraph;
 import Graphs.Models.Edge;
@@ -17,14 +16,6 @@ public class Runner {
         CreateGraph.createUnweightedGraph(graph);
 
         boolean visited[] = new boolean[V];
-
-        /* FOR BFS */
-
-        // for (int i=0; i<V ; i++){
-        //     if(!visited[i]){
-        //         Bfs.bfs(graph, V, visited, i);
-        //     }
-        // }
 
         /* FOR DFS */
 
